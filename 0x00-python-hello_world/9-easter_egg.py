6382e02706a4bf10e38bb54ca9b3746d6af27dc8
Rayd Trujillo <rayd18_93@hotmail.com>
2021-09-07 05:15:32 +0000

    Import this

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,13 +1,2 @@
 #!/usr/bin/python3
-print("The Zen of Python, by Tim Peters\n\nBeautiful is better than ugly.\nExpl\
-icit is better than implicit.\nSimple is better than complex.\nComplex is bette\
-r than complicated.\nFlat is better than nested.\nSparse is better than dense.\
-\nReadability counts.\nSpecial cases aren't special enough to break the rules.\
-\nAlthough practicality beats purity.\nErrors should never pass silently.\n\
-Unless explicitly silenced.\nIn the face of ambiguity, refuse the temptation to\
- guess.\nThere should be one-- and preferably only one --obvious way to do it.\
-\nAlthough that way may not be obvious at first unless you're Dutch.\nNow is be\
-tter than never.\nAlthough never is often better than *right* now.\nIf the im\
-plementation is hard to explain, it's a bad idea.\nIf the implementation is eas\
-y to explain, it may be a good idea.\nNamespaces are one honking great idea -- \
-let's do more of those!")
+import this
